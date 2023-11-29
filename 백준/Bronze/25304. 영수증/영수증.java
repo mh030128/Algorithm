bc8f3680646929e3b84c99bf90a0dd72f348c5fb
sujin Kim <125934630+mh030128@users.noreply.github.com>
2023-11-29 16:50:39 +0900

    [Bronze IV] Title: 영수증, Time: 220 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int total = sc.nextInt();
+		int count = sc.nextInt();
+		int sum = 0;
+		
+		for(int i=0; i<count; i++) {
+			int price = sc.nextInt();
+			int cnt = sc.nextInt();
+			sum += price * cnt;
+		}
+		if (total - sum == 0) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
