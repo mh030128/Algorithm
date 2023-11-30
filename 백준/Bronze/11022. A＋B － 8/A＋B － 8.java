5ad8996691cf5073e48ffa2f5316cfa234f1bf5e
sujin Kim <125934630+mh030128@users.noreply.github.com>
2023-11-30 11:37:12 +0900

    [Bronze V] Title: A+B - 8, Time: 180 ms, Memory: 16476 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,15 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) {
+public class Main {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int t = sc.nextInt();
-		for(int i=1; i<=t; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
+		int T = Integer.parseInt(br.readLine());
+		int a;
+		int b;
+		
+		StringTokenizer st;
+		for(int i=1; i<=T; i++) {
+			st = new StringTokenizer(br.readLine()," ");	// 공백 단위로 입력해 준 문자열을 공백단위로 분리
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
 			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a+b));
 		}
+		br.close();
 	}
 }
