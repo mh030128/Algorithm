969c50c08b68b1a14a74cf2259d5c2dfd1937255
sujin Kim <125934630+mh030128@users.noreply.github.com>
2023-11-30 11:26:06 +0900

    [Bronze V] Title: A+B - 7, Time: 164 ms, Memory: 16156 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) {
+public class Main {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int t = sc.nextInt();
-		for(int i=1; i<=t; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			System.out.println("Case #" + i + ": " + (a+b));
+		int a = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		for(int i=1; i<=a; i++) {
+			st = new StringTokenizer(br.readLine()," ");	// 공백 단위로 입력해 준 문자열을 공백단위로 분리
+			System.out.println("Case #" + i + ": " + 
+			(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
 		}
+		br.close();
 	}
 }
