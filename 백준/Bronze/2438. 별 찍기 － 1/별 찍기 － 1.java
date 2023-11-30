dee029ab8be19568226bcc264daa3c3f13082b37
sujin Kim <125934630+mh030128@users.noreply.github.com>
2023-11-30 13:46:56 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,21 +1,24 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		br.close();
 		
-		StringBuilder sb = new StringBuilder();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		for(int i=1; i<=n; i++) {
 			for(int j=1; j<=i; j++) {
-				sb.append("*");
+				bw.write("*");
 			}
-			sb.append("\n");
+			bw.newLine();
 		}
-		System.out.println(sb);
+		bw.flush();
+		bw.close();
 	}
 }
