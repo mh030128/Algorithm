4870f4690b9c0f596c74efc5af9d211abce0ee10
sujin Kim <125934630+mh030128@users.noreply.github.com>
2023-12-01 17:08:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 580 ms, Memory: 90376 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,26 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
-		
+		Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int index = 0;
-		int[] arr = new int[n];
-		while(st.hasMoreTokens()) {
-			arr[index] = Integer.parseInt(st.nextToken());
-			index++;
-		}
+		int max = -1000001;
+		int min = 1000001;
 		
-		Arrays.sort(arr);
-		System.out.println(arr[0] + " " + arr[arr.length-1]);
+		while(st.hasMoreTokens()) {
+			int value = Integer.parseInt(st.nextToken());
+			if (value > max) {
+				max = value;
+			}
+			if (value < min) {
+				min = value;
+			}
+		}
+		System.out.println(min + " " + max);
 	}
 }
