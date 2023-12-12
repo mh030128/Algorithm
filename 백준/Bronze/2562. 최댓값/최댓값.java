4df1fb4e31acdf6bd45a8a9db812b42e38fb13b6
sujin Kim <125934630+mh030128@users.noreply.github.com>
2023-12-12 14:16:04 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 16000 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-	public static void main(String[] args) {
+public class Main {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int[] arr = new int[9];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
 		int max = 0;
 		int count = 0;
 		
 		for(int i=0; i<arr.length; i++) {
-			arr[i] = sc.nextInt();
 			if (max < arr[i]) {
 				max = arr[i];
-				count = i+1;
+				count = i + 1;
 			}
 		}
 		System.out.println(max + "\n" + count);
