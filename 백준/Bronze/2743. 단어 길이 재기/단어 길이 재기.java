dc21adfaa513ca67ebc0c087fbd58a8d7389861d
sujin Kim <mh030128@naver.com>
2024-11-28 16:28:00 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 172 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str;
+
+        Scanner sc = new Scanner(System.in);
+        str = sc.next();
+
+        System.out.println(str.length());
+    }
+}
