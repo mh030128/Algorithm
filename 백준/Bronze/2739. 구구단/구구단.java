4b5669ebebea7f25f36b847a8666caf6856b9654
sujin Kim <mh030128@naver.com>
2025-04-07 14:55:01 +0900

    [Bronze V] Title: 구구단, Time: 208 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int number = sc.nextInt();
-		
-		for(int i = 1; i<=9; i++) {
-			System.out.println(number + " * " + i + " = " + number * i);
-		}
-	}
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        for(int i = num; i <= num; i++) {
+            for(int j = 1; j < 10; j++) {
+                System.out.println(num + " * " + j + " = " + num*j);
+            }
+            System.out.println();
+        }
+    }
 }
