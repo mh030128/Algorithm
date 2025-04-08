a89192b1ade8bd0504a3b68f4d1a5b260512f4ee
sujin Kim <mh030128@naver.com>
2025-04-08 17:42:23 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,23 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int totalPrice = scanner.nextInt();
-        int totalQuantity = scanner.nextInt();
+        int totalPrice = Integer.parseInt(br.readLine());
+        int totalQuantity = Integer.parseInt(br.readLine());
 
         int total = 0;
 
         for(int i = 0; i < totalQuantity; i++) {
-            int price = scanner.nextInt();
-            int quantity = scanner.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            total += (price * quantity);
+            total += ((Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken())));
+
         }
 
-        if(totalPrice - total == 0) {
+        if (totalPrice - total == 0 ) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
