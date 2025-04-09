2029ff3ec6a84cd843b94c973dea038ff5e257c7
sujin Kim <mh030128@naver.com>
2025-04-09 17:29:55 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,24 +1,24 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		br.close();
-		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=i; j++) {
-				bw.write("*");
-			}
-			bw.newLine();
-		}
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < num; i++) {
+            for(int j = 0; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        String str = sb.toString();
+        System.out.println(str);
+        br.close();
+    }
 }
