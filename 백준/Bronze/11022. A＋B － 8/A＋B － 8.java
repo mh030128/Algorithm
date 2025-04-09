356a2dcb253a690ea85edaec134f0ce4460dc8a4
sujin Kim <mh030128@naver.com>
2025-04-09 17:04:48 +0900

    [Bronze V] Title: A+B - 8, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -4,21 +4,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		int a;
-		int b;
-		
-		StringTokenizer st;
-		for(int i=1; i<=T; i++) {
-			st = new StringTokenizer(br.readLine()," ");	// 공백 단위로 입력해 준 문자열을 공백단위로 분리
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a+b));
-		}
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        for(int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #").append(i+1).append(": ").append(a).append(" + ").append(b).append(" = ").append(a+b).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
