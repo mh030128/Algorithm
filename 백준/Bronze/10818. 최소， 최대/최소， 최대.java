a8c30bc0d67c3a4a2e6bfb82609d1210471136ab
sujin Kim <mh030128@naver.com>
2025-04-16 15:45:53 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 92208 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,25 +4,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int max = -1000001;
-		int min = 1000001;
-		
-		while(st.hasMoreTokens()) {
-			int value = Integer.parseInt(st.nextToken());
-			if (value > max) {
-				max = value;
-			}
-			if (value < min) {
-				min = value;
-			}
-		}
-		System.out.println(min + " " + max);
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
+        int numArr[] = new int[num];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < num; i++) {
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = numArr[0];
+        int min = numArr[0];
+
+        for(int i = 0; i < num; i++) {
+            if(max < numArr[i]) {
+                max = numArr[i];
+            }
+            if(min > numArr[i]) {
+                min = numArr[i];
+            }
+        }
+        System.out.println(min + " " + max);
+        br.close();
+    }
 }
