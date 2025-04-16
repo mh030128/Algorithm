34180dc1ed2e4872b395fb74176da96e82def5a7
sujin Kim <mh030128@naver.com>
2025-04-16 14:23:03 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -3,28 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		int count = 0;
-		int[] arr = new int[n];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int v = Integer.parseInt(br.readLine());
-		for(int i=0; i<n; i++) {
-			if (v == arr[i]) {
-				count++;
-			}
-		}
-		System.out.println(count);
-		
-		br.close();
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int num = Integer.parseInt(br.readLine());
+        int arrayNum[] = new int[num];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < num; i++) {
+            arrayNum[i] = Integer.parseInt(st.nextToken());
+        }
+        int findNum = Integer.parseInt(br.readLine());
+        int count = 0 ;
+
+        for(int i = 0; i < arrayNum.length; i++) {
+            if(findNum == arrayNum[i]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        br.close();
+    }
 }
