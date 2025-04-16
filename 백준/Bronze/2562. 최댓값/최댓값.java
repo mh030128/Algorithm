1ee808f887545bd5775c06448112f062056f9b45
sujin Kim <mh030128@naver.com>
2025-04-16 15:23:22 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 15872 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int numArr[] = new int[9];
         int max = 0;
         int count = 0;
 
-        for(int i = 0; i < numArr.length; i++) {
-            numArr[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < 9; i++) {
+            int num = Integer.parseInt(br.readLine());
 
-            if(max < numArr[i]) {
-                max = numArr[i];
+            if(max < num) {
+                max = num;
                 count = i + 1;
             }
         }
