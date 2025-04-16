0c852d96222160bdde370f952926f1887cbc7668
sujin Kim <mh030128@naver.com>
2025-04-16 14:45:06 +0900

    [Bronze V] Title: X보다 작은 수, Time: 136 ms, Memory: 15428 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -3,25 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		int n = Integer.parseInt(st.nextToken());
-		int x = Integer.parseInt(st.nextToken());
-		
-		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		
-		for(int i=0; i<n; i++) {
-			int value = Integer.parseInt(st.nextToken());
-			
-			if (value < x) {
-				sb.append(value).append(" ");
-			}
-		}
-		System.out.println(sb);
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(st.nextToken());
+        int findNum = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < num; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if(value < findNum) {
+                sb.append(value).append(" ");
+            }
+        }
+        System.out.println(sb);
+        br.close();
+    }
 }
