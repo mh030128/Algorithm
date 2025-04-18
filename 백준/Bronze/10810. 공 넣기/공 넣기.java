f6b01f8558654425ea27f6ee583917ddacda1f2e
sujin Kim <mh030128@naver.com>
2025-04-18 16:19:50 +0900

    [Bronze III] Title: 공 넣기, Time: 216 ms, Memory: 18604 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();  // 바구니 수
+        int M = scanner.nextInt();  // 공 넣기 횟수
+
+        int basket[] = new int[N];
+
+        for(int m = 0; m < M; m++) {
+            int i = scanner.nextInt();
+            int j = scanner.nextInt();
+            int k = scanner.nextInt();
+
+            for(int b = i - 1; b <= j - 1; b++) {
+                basket[b] = k;
+            }
+        }
+        for (int result : basket) {
+            System.out.print(result + " ");
+        }
+        scanner.close();
+    }
+}
