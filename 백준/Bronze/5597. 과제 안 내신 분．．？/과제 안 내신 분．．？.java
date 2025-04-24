9518d77f2e74cfee2c97ef8eaa2f1b12280f2e9d
sujin Kim <mh030128@naver.com>
2025-04-24 16:37:34 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,22 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        Set<Integer> students = new HashSet<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean[] student = new boolean[31];
+
+        for(int i = 1; i <= 30; i++) {
+            students.add(i);
+        }
 
         for(int i = 0; i < 28; i++) {
             int stuNum = Integer.parseInt(br.readLine());
-            student[stuNum] = true;
+            students.remove(stuNum);
         }
 
-        for(int i = 1; i <= 30; i++) {
-            if(!student[i]) {
-                System.out.println(i);
-            }
+        List<Integer> result = new ArrayList<>(students);
+        Collections.sort(result);
+
+        for (Integer n : result) {
+            System.out.println(n);
         }
     }
 }
