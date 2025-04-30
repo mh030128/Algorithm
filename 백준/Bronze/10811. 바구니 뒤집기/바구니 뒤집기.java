a52251b766f9fd2fc20aef19478fe14f061f8052
sujin Kim <mh030128@naver.com>
2025-04-30 16:21:37 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 124 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,26 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-
-        int basketNum = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int basketNum = Integer.parseInt(st.nextToken());
         int[] basket = new int[basketNum];
-        int inverseNum = scanner.nextInt();
+        int inverseNum = Integer.parseInt(st.nextToken());
 
         for(int i = 0; i < basket.length; i++) {
             basket[i] = i + 1;
         }
 
         for(int i = 0; i < inverseNum; i++) {
-            int num1 = scanner.nextInt() - 1;
-            int num2 = scanner.nextInt() - 1;
+            st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken()) - 1;
+            int num2 = Integer.parseInt(st.nextToken()) - 1;
 
             while(num1 < num2) {
                 int temp = basket[num1];
                 basket[num1] = basket[num2];
                 basket[num2] = temp;
+
                 num1++;
                 num2--;
             }
