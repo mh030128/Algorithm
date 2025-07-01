f4511f6b75fa63bb1264684aa06e0b0822c28bad
sujin Kim <mh030128@naver.com>
2025-07-01 17:01:58 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 192 ms, Memory: 18316 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.next();
+        int[] positions = new int[26];
+
+        for(int i = 0; i < positions.length; i++) {
+            positions[i] = -1;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            char target = s.charAt(i);
+            int index = target - 'a';
+            if(positions[index] == -1) {
+                positions[index] = i;
+            }
+        }
+
+        for(int i = 0; i < positions.length; i++) {
+            System.out.print(positions[i] + " ");
+        }
+    }
+}
