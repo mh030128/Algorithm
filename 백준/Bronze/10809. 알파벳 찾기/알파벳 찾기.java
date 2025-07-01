008f09b19d9a4e88893c274437ec3c869196a377
sujin Kim <mh030128@naver.com>
2025-07-01 17:13:02 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,27 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-
-        String s = scanner.next();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
         int[] positions = new int[26];
 
-        for(int i = 0; i < positions.length; i++) {
+        for (int i = 0; i < positions.length; i++) {
             positions[i] = -1;
         }
 
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i <s.length(); i++) {
             char target = s.charAt(i);
             int index = target - 'a';
             if(positions[index] == -1) {
                 positions[index] = i;
             }
         }
-
+        
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < positions.length; i++) {
-            System.out.print(positions[i] + " ");
+            sb.append(positions[i]).append(" ");
         }
+        System.out.println(sb);
     }
 }
