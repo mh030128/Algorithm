babe09d9295d27c6c169ff0ec352dd6eecf2945a
sujin Kim <mh030128@naver.com>
2025-08-08 17:43:10 +0900

    [Bronze II] Title: 다이얼, Time: 164 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        int total = 0;
+
+        for(int i = 0; i <input.length(); i++) {
+            char ch = input.charAt(i);
+
+            switch (ch) {
+                case 'A': case 'B': case 'C':
+                    total += 3;
+                    break;
+                case 'D': case 'E': case 'F':
+                    total += 4;
+                    break;
+                case 'G': case 'H': case 'I':
+                    total += 5;
+                    break;
+                case 'J': case 'K': case 'L':
+                    total += 6;
+                    break;
+                case 'M': case 'N': case 'O':
+                    total += 7;
+                    break;
+                case 'P': case 'Q': case 'R': case'S':
+                    total += 8;
+                    break;
+                case 'T': case 'U': case 'V':
+                    total += 9;
+                    break;
+                case 'W': case 'X': case 'Y': case 'Z':
+                    total += 10;
+                    break;
+            }
+        }
+        System.out.println(total);
+    }
+}
