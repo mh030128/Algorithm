1d6ce0e8ad6f1781c700a2f591580ff5b18af78c
sujin Kim <mh030128@naver.com>
2025-08-08 22:02:29 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -1,14 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> lines = new ArrayList<>();
         String input;
 
         while((input = br.readLine()) != null) {
-            System.out.println(input);
+            lines.add(input);   // 배열에 한 줄씩 추가
+        }
+
+        for (String str : lines) {
+            System.out.println(str);
         }
     }
 }
