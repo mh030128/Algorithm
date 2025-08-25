f89cd177cd1ba7dc98ddce3666418d3da94781e8
sujin Kim <mh030128@naver.com>
2025-08-25 16:29:37 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 116 ms, Memory: 15852 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,15 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         int[] correct = {1, 1, 2, 2, 2, 8};
         int[] input = new int[6];
 
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < input.length; i++) {
-            input[i] = scanner.nextInt();
+            input[i] = Integer.parseInt(st.nextToken());
         }
 
         for(int i = 0; i < input.length; i++) {
