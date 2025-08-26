3be6ce99cffdfc773e2d92e8b6b375c56206bdb9
sujin Kim <mh030128@naver.com>
2025-08-26 16:26:07 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,21 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String input = br.readLine();
         StringBuffer sb = new StringBuffer(input);
         String reverse = sb.reverse().toString();
 
-        if(input.equals(reverse)) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+        System.out.println(input.equals(reverse) ? 1 : 0);
     }
 }
