c52c358a6a534cb276a6c5342ae1acf7705c9ad7
sujin Kim <mh030128@naver.com>
2025-08-26 16:22:52 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String result = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i = input.length()-1; i >= 0; i--) {
-            result += input.charAt(i);
-        }
+        String input = br.readLine();
+        StringBuffer sb = new StringBuffer(input);
+        String reverse = sb.reverse().toString();
 
-        if(input.equals(result)) {
+        if(input.equals(reverse)) {
             System.out.println(1);
         } else {
             System.out.println(0);
