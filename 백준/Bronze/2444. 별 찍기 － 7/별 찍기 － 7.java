e146d32aebd726bc11188324b6154e9892b27dd7
sujin Kim <mh030128@naver.com>
2025-08-27 01:18:19 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -6,33 +6,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
 
-        // 1. 위쪽 삼각형
-        for (int i = 1; i <= n; i++) {
-            // 공백출력
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            // 별출력
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        // 위쪽삼각형
+        for(int i = 1; i <= n; i++) {
+            sb.append(" ".repeat(n - i))    // 공백출력
+                .append("*".repeat(2 * i - 1))  // 별출력
+                    .append("\n");
         }
 
-        // 2. 아래쪽 삼각형
-        for (int i = n - 1; i >= 1; i--) {
-            // 공백출력
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            // 별출력
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        // 아래쪽삼각형
+        for(int i = n - 1; i >= 1; i--) {
+            sb.append(" ".repeat(n - i))    // 공백출력
+                .append("*".repeat(2 * i - 1))  // 별출력
+                    .append("\n");
         }
-        br.close();
+            
+        // 최종출력
+        System.out.println(sb);
     }
+
 }
