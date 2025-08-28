e666c91cf2b45a920759a9c1e4d0f8fe2bf1dd66
sujin Kim <mh030128@naver.com>
2025-08-28 14:32:56 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 176 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String word = scanner.next();
+        String word = scanner.nextLine();
         String[] croatia = new String[]{"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-        int cnt = 0;
 
         for (String pattern : croatia) {
             word = word.replace(pattern, "#");
