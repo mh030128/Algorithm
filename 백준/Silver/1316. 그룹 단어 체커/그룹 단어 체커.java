d9199e1bf3e15eae391fc212a3b98872b20dadf4
sujin Kim <mh030128@naver.com>
2025-09-01 17:19:03 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,33 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            String word = scanner.next();
-            if (isGroupWord(word)) {
+        for(int i = 0 ; i < n; i++) {
+            String word = br.readLine();
+            if(isGroupWord(word)) {
                 count++;
             }
         }
         System.out.println(count);
     }
+    
     public static boolean isGroupWord(String word) {
-        boolean[] visited = new boolean[26];    /* boolean 초기값 = false */
+        boolean[] visit = new boolean[26];
         char prev = 0;
 
         for(int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-
             if(ch != prev) {
-                if(visited[ch - 'a']) {     /* false이면 실행하지 않고, 다음 문장으로 실행 */
+                if(visit[ch - 'a']) {
                     return false;
                 }
-                visited[ch - 'a'] = true;
+                visit[ch - 'a'] = true;
             }
             prev = ch;
         }
