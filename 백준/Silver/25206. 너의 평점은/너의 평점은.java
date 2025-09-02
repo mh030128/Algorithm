de7f6563a0d1a6ee20f11b0d4132e46883a809bc
sujin Kim <mh030128@naver.com>
2025-09-02 17:04:56 +0900

    [Silver V] Title: 너의 평점은, Time: 112 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,41 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<String, Double> scoreMap = new HashMap<>();
-        StringTokenizer st;
 
-        double totalScore = 0.0;
-        double totalScoreP = 0.0;
-
-        scoreMap.put("A+", 4.5);
-        scoreMap.put("A0", 4.0);
-        scoreMap.put("B+", 3.5);
-        scoreMap.put("B0", 3.0);
-        scoreMap.put("C+", 2.5);
-        scoreMap.put("C0", 2.0);
-        scoreMap.put("D+", 1.5);
-        scoreMap.put("D0", 1.0);
-        scoreMap.put("F", 0.0);
+        double totalScore = 0.0;    /* 학점 * 과목평점 */
+        double totalScoreP = 0.0; /* P제외 점수 */
 
         for(int i = 0; i < 20; i++) {
-            st = new StringTokenizer(br.readLine());
-            String subject = st.nextToken();
-            double score = Double.parseDouble(st.nextToken());
-            String grade = st.nextToken();
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            if(!grade.equals("P")) {
-                totalScore += (score * scoreMap.get(grade));
-                totalScoreP += score;
+            st.nextToken(); /* 과목명은 계산에 사용하지 않으므로 바로 넘김. */
+            double score = Double.parseDouble(st.nextToken()); /* 학점 */
+            String grade = st.nextToken(); /* 등급 */
+
+            double majorScore = 0.0; /* 과목평점 */
+            switch (grade) {
+                case "A+" : majorScore = 4.5; break;
+                case "A0" : majorScore = 4.0; break;
+                case "B+" : majorScore = 3.5; break;
+                case "B0" : majorScore = 3.0; break;
+                case "C+" : majorScore = 2.5; break;
+                case "C0" : majorScore = 2.0; break;
+                case "D+" : majorScore = 1.5; break;
+                case "D0" : majorScore = 1.0; break;
+                case "F" : majorScore = 0.0; break;
+                case "P" : continue;    /* P학점 제외 */
             }
+            totalScore += (score * majorScore);
+            totalScoreP += score;
         }
-        double result = totalScore / totalScoreP;
-        System.out.printf("%.6f\n", result);
+        System.out.printf("%.6f\n", totalScore / totalScoreP);
     }
 }
