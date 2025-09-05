01a772907e8c98ccd03a3c8472ac56b5972a0fa5
sujin Kim <mh030128@naver.com>
2025-09-05 15:21:20 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,36 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
+    static private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int[][] arr = new int[9][9];
-
         int max = 0;
-        int x = 1, y = 1;
+        int maxRow = 1, maxCol = 1;
 
         for(int i = 0; i < arr.length; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j <arr[0].length; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            String line = br.readLine();
+            String[] token = line.split(" ");
+            for(int j = 0; j < token.length; j++) {
+                arr[i][j] = Integer.parseInt(token[j]);
             }
         }
-
         for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr[0].length; j++) {
+            for(int j = 0; j <arr[0].length; j++) {
                 if(arr[i][j] > max) {
                     max = arr[i][j];
-                    x = i + 1;
-                    y = j + 1;
+                    maxRow = i + 1;
+                    maxCol = j + 1;
                 }
             }
         }
-        sb.append(max).append("\n").append(x).append(" ").append(y);
-        System.out.println(sb);
+        StringBuilder sb = new StringBuilder();
+        sb.append(max).append("\n").append(maxRow).append(" ").append(maxCol);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
