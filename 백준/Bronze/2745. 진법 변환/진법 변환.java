8f01dae54612a705563fa576565ca4fee1970355
sujin Kim <mh030128@naver.com>
2025-09-12 18:18:38 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -8,23 +8,18 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         String n = st.nextToken();
         int b = Integer.parseInt(st.nextToken());
         int sum = 0;
+        int d = 1;
 
-        for(int i = 0; i < n.length(); i++) {
-            char c = n.charAt((n.length() - 1) - i);
-            int result;
-            if (c >= 'A' && c <= 'Z') {
-                result = c - 55;
-            } else {
-                result = c - '0';
-            }
-            sum += (int) Math.pow(b, i) * result;
+        for(int i = n.length() - 1; i >= 0; i--) {
+            char c = n.charAt(i);
+            int result = c >= 'A' && c <= 'Z' ? c - 55 : c - '0';
+            sum += result * d;
+            d = d * b;
         }
-        sb.append(sum);
-        System.out.println(sb);
+        System.out.println(sum);
     }
 }
